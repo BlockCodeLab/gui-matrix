c63b33bac1248a423f38079635f8a090b7209399
Yeqin Gong <gon@gyeq.in>
2024-09-03 11:22:56 +0800

    add tutorials template

---
+++ src/tutorials/getting-started/getting-started.jsx
@@ -0,0 +1,28 @@
+import { Text } from '@blockcode/ui';
+
+export default {
+  id: 'getting-started',
+  title: (
+    <Text
+      id="arcade.tutorials.gettingStarted"
+      defaultMessage="Getting Started"
+    />
+  ),
+  image: 'https://scratch.mit.edu/static/assets/2d87300fabb15fd7c2ef59f9ca2fc921.jpg',
+  // tags: [],
+  pages: [
+    {
+      title: 'lesson1-page1',
+      image: 'https://scratch.mit.edu/static/assets/464e70f97462e5e5d019391794ec31f8.gif',
+    },
+    {
+      title: 'lesson1-page2',
+      image: 'https://scratch.mit.edu/static/assets/d5937003d51d262ff9cae01ca0f86a2f.gif',
+    },
+    {
+      title: 'lesson1-page3',
+      text: 'text',
+    },
+  ],
+  // next: ['lesson2', 'lesson3'],
+};
---
+++ src/tutorials/tutorials.jsx
@@ -0,0 +1,13 @@
+import gettingStarted from './getting-started/getting-started';
+
+export default {
+  // tags: [
+  //   {
+  //     tag: '',
+  //     label: '',
+  //   },
+  // ],
+  lessons: {
+    [gettingStarted.id]: gettingStarted,
+  },
+};
