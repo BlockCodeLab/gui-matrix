76971cab32fcf35cbc58b37f9ef22b400b956831
kankungyip <kankungyip@users.noreply.github.com>
2024-10-23 23:01:34 +0800

    change image type for download

--- src/lib/generate-image.js
+++ src/lib/generate-image.js
@@ -1,14 +1,5 @@
 import UPNG from 'upng-js';
-import imageContour, { ALPHA_THRESHOLD } from './image-contour';
-
-const color16 = (r, g, b) => {
-  if (r === 0 && g === 0 && b === 0) {
-    r = 0;
-    g = 4;
-    b = 0;
-  }
-  return ((r & 0xf8) << 8) | ((g & 0xfc) << 3) | (b >> 3);
-};
+import imageContour from './image-contour';
 
 const base64ToArrayBuffer = (base64String) => {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
@@ -32,23 +23,8 @@ export default function ({ id, name, type, data, width, height, centerX, centerY
   });
   const contourData = JSON.stringify(contour).replaceAll('[', '(').replaceAll(']', ',)').replaceAll('(,)', '()');
 
-  const imageData = [];
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
-      const i = (y * width + x) << 2;
-      const r = rgba[i + 0];
-      const g = rgba[i + 1];
-      const b = rgba[i + 2];
-      const a = rgba[i + 3];
-      const color = a < ALPHA_THRESHOLD ? 0x0000 : color16(r, g, b);
-      imageData.push(color & 0xff); // low
-      imageData.push((color >> 8) & 0xff); // high
-    }
-  }
-
-  const moduleName = `image${id}`;
-
   let imageModule = '';
+  imageModule += 'from scratch import *\n';
   imageModule += 'from micropython import const\n';
   imageModule += `ID = "${id}"\n`;
   imageModule += `NAME = "${name}"\n`;
@@ -58,19 +34,17 @@ export default function ({ id, name, type, data, width, height, centerX, centerY
   imageModule += `CENTER_Y = const(${centerY})\n`;
   imageModule += `CONTOUR = ${contourData}\n`;
   imageModule += `dirpath = '/'.join(__file__.split('/')[0:-1])\n`;
-  imageModule += `f = open(f"{dirpath}/${id}", "b")\n`;
-  imageModule += `_BITMAP = f.read()\n`;
-  imageModule += `f.close()\n`;
-  imageModule += `BITMAP = memoryview(_BITMAP)\n`;
+  imageModule += `res = runtime.display._lcd.png_decode(f"{dirpath}/${id}.png")\n`;
+  imageModule += `BITMAP = memoryview(res[0])\n`;
 
   return [
     {
-      id,
+      id: `${id}.png`,
       type,
-      content: Uint8Array.from(imageData),
+      content: buffer, // Uint8Array.from(imageData),
     },
     {
-      id: moduleName,
+      id: `image${id}`,
       type: 'text/x-python',
       content: imageModule,
     },
