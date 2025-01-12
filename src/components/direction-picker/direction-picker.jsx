918a8d41b4addcb7a0f9445013abec6e15286211
Yeqin Gong <gon@gyeq.in>
2025-01-12 23:02:06 +0800

    优化鼠标事件

--- src/components/direction-picker/direction-picker.jsx
+++ src/components/direction-picker/direction-picker.jsx
@@ -44,11 +44,11 @@ export function DirectionPicker({ direction, rotationStyle, children, onChange, 
       onChange(newDirection);
     };
     const mouseUp = () => {
-      document.removeEventListener('mousemove', mouseMove);
-      document.removeEventListener('mouseup', mouseUp);
+      document.removeEventListener('pointermove', mouseMove);
+      document.removeEventListener('pointerup', mouseUp);
     };
-    document.addEventListener('mousemove', mouseMove);
-    document.addEventListener('mouseup', mouseUp);
+    document.addEventListener('pointermove', mouseMove);
+    document.addEventListener('pointerup', mouseUp);
   }, []);
 
   return (
